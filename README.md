bdf30e6da624df69f2ecd71a05d6ae32623fae89
YoungHyeong Ryu <wangta69@naver.com>
2018-05-30 17:53:00 +0900

    angular5 to 6
    angular5 to 6

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # ng-functions
-Tested for angular5
+Tested for angular6
 
 ## Installation
 ```
 npm install ng-functions
 ```
+
+## Installation [angular 5.x]
+```
+npm install ng-functions@1.0.1
+```
+
 ## How to use
 
 ### imports
@@ -19,4 +25,10 @@ import { CommonService }  from "ng-functions";
 constructor(protected commonSvc: CommonService) {
 let prefix_unique = commonSvc.guid();
 }
+```
+
+```
+randomString(number);//return alphaNumeric random string as given number length;
+in_array(needle, haystack);//if needle is in array return true else return false
+array_search(needle, haystack);//if needle is in array return index else return false
 ```
--- src/common.service.ts
+++ src/common.service.ts
@@ -5,17 +5,63 @@ export class CommonService {
 	constructor() {
 	}
 
+	guid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+	};
+
+	/**
+	* create random string as given length
+	*/
+	randomString(len) {
+      let text = "";
+      let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for (var i = 0; i < len; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+	};
+
 	range(input, min:number, max:number): any{//@Pipe({name: 'range'}) 와 동일 (range.pipe.ts)
 		for (var i=min; i<=max; i++)
 			input.push(i);
 		return	input;
 	};//init:function(){
 
-	guid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
+	in_array(needle, haystack) {
+	    for(var i in haystack) {
+	        if(haystack[i] == needle) return true;
+	    }
+	    return false;
 	};
+
+	copy_array(arr){
+		return arr.slice();
+	}
+
+	/**
+	* remove item from array by value
+	*/
+	remove_item(value, haystack){
+		return haystack.filter(v => v != value);
+	}
+
+	array_search(needle, haystack) {
+	    for(var i in haystack) {
+	        if(haystack[i] == needle) return i;
+	    }
+	    return false;
+	};
+
+	add_comma(input){
+		return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+
+	rm_comma(input){
+		return input.toString().replace(/,/g , "");
+	}
 
 }
