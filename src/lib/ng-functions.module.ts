09d2371e6fb4cfe1e6dcb26401e9fbb1ce85b8dd
YoungHyeong Ryu <wangta69@naver.com>
2018-07-17 11:09:51 +0900

    t
    t

--- src/lib/ng-functions.module.ts
+++ src/lib/ng-functions.module.ts
@@ -3,19 +3,8 @@ import { CommonService } from './common.service';
 @NgModule({
   imports: [
   ],
-  providers: [CommonService],
+  providers: [ CommonService ],
   declarations: [],
   exports: []
 })
 export class CommonServiceModule { }
-/*
-import { NgModule } from '@angular/core';
-import { NgFunctionsComponent } from './ng-functions.component';
-@NgModule({
-  imports: [
-  ],
-  declarations: [NgFunctionsComponent],
-  exports: [NgFunctionsComponent]
-})
-export class NgFunctionsModule { }
-*/
